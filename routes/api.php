0578f55eafb14a0b26994371951782ed77bc0f7a
Nguyễn Huy Hiệu <hieunh161@gmail.com>
2018-02-20 10:18:31 +0900

    add authorization and dummy user for testing

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function () {
+    return response([1,2,3,4], 200);
+})->middleware('auth:api');
